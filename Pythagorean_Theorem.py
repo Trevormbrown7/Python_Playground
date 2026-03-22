8731837a15763e686ce2706ec72e9e02e990ffd9
Trevor Brown <Trevormbrown7@gmail.com>
2026-03-22 10:30:17 -0500

    edits to Pythagorean_Theorem.py

--- Pythagorean_Theorem.py
+++ Pythagorean_Theorem.py
@@ -9,6 +9,13 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
+def draw_circle():
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    r = 200
+    pygame.draw.circle(screen, ("green"), (x, y), r)
+    return x, y, r
+
 while running:
     mouse_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
@@ -17,8 +24,13 @@ while running:
 
     screen.fill("black")
 
-    pygame.draw.circle(screen, ("green"), (SCREEN_WIDTH//2, SCREEN_HEIGHT//2), 200)
-    pygame.draw.line(screen, ("white"), (SCREEN_WIDTH//2, SCREEN_HEIGHT//2), (mouse_pos[0], mouse_pos[1]), 2)
+    x, y, r = draw_circle()
+    pygame.draw.line(screen, ("white"), (x, y), (mouse_pos[0], mouse_pos[1]), 2)
+    pygame.draw.line(screen, ("white"), (x, y), (x-r, y), 2)
+    pygame.draw.line(screen, ("white"), (x, y), (x, y-r), 2)
+    #x2 + y2 = z2
+    z = (x**2 + y**2)**0.5
+    pygame.draw.line(screen, ("white"), (x, y), (z, z), 2)
 
     pygame.display.flip()
     clock.tick(60)
