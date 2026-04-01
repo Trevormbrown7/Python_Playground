6c12c2f51627ba0c56006251730f1b017b7313fa
Trevor Brown <Trevormbrown7@gmail.com>
2026-04-01 09:41:40 -0500

    Updated - MacbookPro

--- Clicker Game/Clicker_Game.py
+++ Clicker Game/Clicker_Game.py
@@ -26,7 +26,7 @@ class Button:
         pygame.draw.rect(screen, self.color, self.rect)
         t = self.font.render(self.text, True, (255, 255, 255))
         text_rect = t.get_rect(center=self.rect.center)
-        screen.blit(t, (text_rect))
+        screen.blit(t, text_rect)
 
     def update(self):
         mouse_pos = pygame.mouse.get_pos()
@@ -40,16 +40,35 @@ class Button:
     def handle_event(self, score, click_power):
         mouse_pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
+            autoclick.can_afford()
             score += click_power
         return score
 
-    def upgrade(self, score, click_power):
+    def upgrade(self, score, auto_clicker, cost):
         mouse_pos = pygame.mouse.get_pos()
         if self.rect.collidepoint(mouse_pos):
-            if score >= 10:
-                click_power *= 10
-                score -= 10
-        return score, click_power
+            if score >= cost:
+                score -= cost
+                if auto_clicker == 0:
+                    auto_clicker += 0.01
+                else: auto_clicker *= 2
+                cost += 5
+        return score, auto_clicker, cost
+
+class Upgrade:
+    def __init__(self, name, cost, cost_multiplier, level, effect):
+        self.name = name
+        self.cost = cost
+        self.cost_multiplier = cost_multiplier
+        self.level = level
+        self.effect = effect
+
+    def can_afford(self, score):
+        if score >= self.cost:
+            print("can afford")
+
+    def purchase(self):
+        pass
 
 # Functions
 def draw_score(screen, text, font, color, center):
@@ -66,18 +85,21 @@ def main():
     pygame.display.set_caption("Clicker Game")
     running = True
     clock = pygame.time.Clock()
-    click_power = 5
+    click_power = 0.5
+    auto_clicker = 0
+    cost = 5
 
     # Instances
     clicker_button = Button(RED, (0, 0, 200, 200), "", 22)
     clicker_button.rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
-    upgrade_button = Button(RED, (2, 100, 150, 70), "Upgrade #1", 17)
-    upgrade_button_02 = Button(RED, (2, 175, 150, 70), "Upgrade #2", 17)
+    upgrade_button = Button(BLUE, (2, 100, 150, 70), "Auto Clicker", 17)
+    upgrade_button_02 = Button(BLUE, (2, 175, 150, 70), "Upgrade #2", 17)
+    autoclick = Upgrade("Auto Click", 10, 2)
 
     # Variables
     buttons = [clicker_button, upgrade_button, upgrade_button_02]
     score = 0
-    score_font = pygame.font.Font(os.path.join("Roboto", "Robo.ttf"), 25)
+    score_font = pygame.font.Font(os.path.join("Roboto", "Roboto-Bold.ttf"), 25)
 
     while running:
         for event in pygame.event.get():
@@ -85,12 +107,16 @@ def main():
                 running = False
             if event.type == MOUSEBUTTONDOWN:
                 score = clicker_button.handle_event(score, click_power)
-                score, click_power = upgrade_button.upgrade(score, click_power)
+                score, auto_clicker, cost = upgrade_button.upgrade(score, auto_clicker, cost)
 
         screen.fill((255,255,255))
 
-        draw_score(screen, f"Score: {score}", score_font, (0, 0, 0),
+        draw_score(screen, f"${score:,.2f}", score_font, (0, 0, 0),
                   (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 300))
+        draw_score(screen, f"Cost: {cost} – Auto Rate: {auto_clicker}", score_font, (0, 0, 0),
+                  (200, 50))
+
+        score += auto_clicker
 
         # Loop through all the buttons... Update, then draw them
         for button in buttons:
@@ -106,10 +132,10 @@ def main():
         pygame.display.flip()
         clock.tick(60)
 
+    pygame.quit()
+    sys.exit()
+
 # Entry Guard
 if __name__ == '__main__':
     main()
-
-pygame.quit()
-sys.exit()
 
